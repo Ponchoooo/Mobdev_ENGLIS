b7f60afb2b84ee3d46f9e4f44df9bc3d3ebf3bf1
Clint Laurence Englis <clintenglis16@gmail.com>
2026-02-09 00:06:06 +0800

    Final Logic done

--- app/src/main/java/com/example/study_buddy/MainActivity.kt
+++ app/src/main/java/com/example/study_buddy/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.study_buddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val nextButton = findViewById<Button>(R.id.button)
+
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/study_buddy/MainActivity3.kt
+++ app/src/main/java/com/example/study_buddy/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.study_buddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity3 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val nextButton = findViewById<Button>(R.id.button)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
         }
     }
 }
